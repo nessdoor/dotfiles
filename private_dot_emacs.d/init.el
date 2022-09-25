2272aa925e913580676f01aea6d16bd0920c959d
Tomas Antonio Lopez <entropy.overseer@entropic.network>
2022-09-25 11:55:44 +0900

    Turn on byte compilation for the tangled init file

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -1,2 +1,2 @@
 ;; Tangle the main configuration file
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory) t)
